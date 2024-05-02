f51457cc49edbedbfd434e752fd129e8f9e2dd88
Ayaz Lakho <ayazalilakho2@gmail.com>
2024-05-03 00:56:18 +0500

    updates

---
+++ LeetCode Problems/LC121_besttimes_to_buy_and_sell.py
@@ -0,0 +1,13 @@
+def maxProfit(prices):
+    profit = 0
+    buy = prices[0]
+    for sell in prices[1:]:
+        if sell > buy:
+            profit = max(profit, sell - buy)
+        else:
+            buy = sell
+    return profit
+
+
+prices = [7,1,5,3,6,4]
+print(maxProfit(prices))
